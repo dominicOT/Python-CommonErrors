7e7ceb8277159f2213a0e7cd91e7b20e5f2cf151
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2022-10-29 23:34:55 +0100

    Add files via upload

---
+++ NameError - 6469.py
@@ -0,0 +1,17 @@
+#Dominic Oladapo-Tonade - 6469
+
+name = "Lua"
+
+print(nmae) #name has been mispelt
+
+def codeWar():
+    print("CodeTitans")
+
+codeWar()
+codeWarriors() #function yet to be defined
+
+d = 10
+
+print(math.cos(d)) #math is not imported
+
+
