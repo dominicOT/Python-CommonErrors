f190e835faa8f57c1ad8439f4800199fa38039e2
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2022-10-29 23:30:37 +0100

    Add files via upload

---
+++ KeyError - 6469.py
@@ -0,0 +1,12 @@
+ages = {'Dan': 17, 'Lucy': 18, 'Maze': 16, 'Eve': 19}
+print({ages['Sam']})
+
+"""
+    output:
+
+    Traceback (most recent call last):
+  File "C:/.../KeyError - 6469.py", line 2, in <module>
+    print({ages['Sam']})
+KeyError: 'Sam'
+
+"""
