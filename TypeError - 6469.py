75b28ea55f47d0fe050a9c3d165aa8c715ae7cdc
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2022-10-30 02:25:22 +0100

    Add files via upload

---
+++ TypeError - 6469.py
@@ -0,0 +1,11 @@
+#Dominic Oladapo-Tonade - 6469
+
+numA = 10
+stringA = "PyDev"
+result = numA + stringA
+
+"""
+ File "C:/..../TypeError - 6469.py", line 5, in <module>
+    result = numA + stringA
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+"""
