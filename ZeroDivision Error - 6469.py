b607f5243025dce3e4bf4751d4f7699b6425f8a4
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2022-11-01 06:23:34 +0100

    Add files via upload

---
+++ ZeroDivision Error - 6469.py
@@ -0,0 +1,15 @@
+#Dominic Oladapo-Tonade - 6469
+
+numA = 10 / 0
+
+print(numA)
+
+"""
+    output:
+
+    Traceback (most recent call last):
+  File "C:/.../ZeroDivision Error - 6469.py", line 3, in <module>
+    numA = 10 / 0
+ZeroDivisionError: division by zero
+
+"""
