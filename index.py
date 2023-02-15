371fb98ea247c25594f9116d4f9f316c6523e790
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-02-15 04:40:43 +0100

    Add files via upload
    index error

---
+++ index.py
@@ -0,0 +1,8 @@
+fruits = list(['apple', 'banns', 'pear', 'mang', 'lmao'])
+print(fruits[6])
+
+
+"""Traceback (most recent call last):
+File "C:\Users\dominic_the_genius\OneDrive\Documents\LIBRARY\UNIMAK\YEAR 3 SEMESTER 5\PYTHON\assignment\index.py", line 2, in <module>
+print(fruits[6])
+IndexError: list index out of range"""
